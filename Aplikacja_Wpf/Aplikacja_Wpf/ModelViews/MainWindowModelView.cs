7a1bc95a1c2f73052cd248b400d5fb8643de72fe
kofi <kqfi26@gmail.com>
2023-04-13 14:51:23 +0200

    MainWindowModelView.cs changed

--- Aplikacja_Wpf/Aplikacja_Wpf/ModelViews/MainWindowModelView.cs
+++ Aplikacja_Wpf/Aplikacja_Wpf/ModelViews/MainWindowModelView.cs
@@ -14,7 +14,7 @@ namespace Aplikacja_Wpf.ModelViews
     {
         public MainWindowModelView()
         {
-            IloczynCommand = new RelayCommand(iloczyn);
+            NWDCommand = new RelayCommand(NWD);
         }
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged(string propertyName)
@@ -31,12 +31,45 @@ namespace Aplikacja_Wpf.ModelViews
                 OnPropertyChanged(nameof(Arg1));
             }
         }
-        public ICommand IloczynCommand { get; set; }
-        public void iloczyn(Object obj)
+
+        private string arg2;
+        public string Arg2
+        {
+            get { return arg2; }
+            set
+            {
+                arg2 = value;
+                OnPropertyChanged(nameof(Arg2));
+            }
+        }
+        private string output;
+
+        public string Output
+        {
+            get { return output; }
+            set
+            {
+                output = value;
+                OnPropertyChanged(nameof(output));
+            }
+        }
+        public ICommand NWDCommand { get; set; }
+        public void NWD(Object obj)
         {
             try
             {
+                double x = Convert.ToDouble(Arg1);
+                double y = Convert.ToDouble(Arg2);
 
+                while(x != y) 
+                {
+                    if(x > y)
+                        x -= y;
+                    else
+                        y -= x;
+                }
+
+                output = x.ToString();
             }
             catch (Exception ex)
             {
